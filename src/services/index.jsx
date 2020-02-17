0f6f6cfa9ff2ba3b6464fc6d2a418259e61b6685
Niurmiguel <niurmiguelg@gmail.com>
2020-02-16 22:21:36 -0400

    Change to https

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,5 +1,5 @@
 const getNews = async ( category = 'general', size = 10 ) => {
-    const url = `http://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=${size}&apiKey=329632789732429d8a59abefaf89e88a`
+    const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&pageSize=${size}&apiKey=329632789732429d8a59abefaf89e88a`
 
   const response = await fetch(url);
   const { articles, status, message } = await response.json();
